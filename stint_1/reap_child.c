eb1dcb8b00bf9bdb8ba13e17331fdf9ca70244ad
Anmol Agarwal <anmolagarwal4453@gmail.com>
2020-10-01 05:58:17 +0530

    Added messgae to be printed when process stopped code

--- stint_1/reap_child.c
+++ stint_1/reap_child.c
@@ -4,7 +4,7 @@
 void reapChild(int signum)
 {
     int i;
-    int *stat_loc = (int *)malloc(sizeof(int));
+    int stat_loc;
     printf("Entered reap Child\n");
     if (getpid() != script_pid)
     {
@@ -14,8 +14,8 @@ void reapChild(int signum)
     while (true)
     {
         //printf("IN LOOP\n");
-        pid_t suspected_pid = waitpid(-1, stat_loc, WNOHANG);
-        printf("Suspected pid is %d\n",suspected_pid);
+        pid_t suspected_pid = waitpid(-1, &stat_loc, WNOHANG | WUNTRACED);
+        printf("Suspected pid is %d\n", suspected_pid);
         if (suspected_pid <= 0)
         {
             /*if WNOHANG was specified and one or more
@@ -36,27 +36,47 @@ void reapChild(int signum)
                     {
                         //possible change in behaviour or has exited
 
-
-
-
-                        //iF process terminated, 
+                        //iF process terminated,
                         //print apt message and make sure that is not considered again
 
-                        //else, proces has undergone a change in status, update the change in status accrdingly, 
+                        //else, proces has undergone a change in status, update the change in status accrdingly,
                         //also print about the change in status
 
                         //////////////////////////////////////////////////////////
                         // [<jid>] (<pid>) terminated with exit status <status>
                         //////////////////////////////////////////////////////////////
-                        if (WIFEXITED(*stat_loc) && WEXITSTATUS(*stat_loc) == EXIT_SUCCESS)
+                        if (WIFEXITED(stat_loc))
                         {
-                            //  https://stackoverflow.com/questions/39002052/how-i-can-print-to-stderr-in-c
-                            jobs_ptr[i]->is_relevant=0;
-                            fprintf(stderr, "%s with pid [%d] and jid [%d] exited normally\n", jobs_ptr[i]->cmd_name, jobs_ptr[i]->pid, jobs_ptr[i]->jid);
+                            jobs_ptr[i]->is_relevant = 0;
+
+                            if (WEXITSTATUS(stat_loc) == EXIT_SUCCESS)
+                            {
+                                //  https://stackoverflow.com/questions/39002052/how-i-can-print-to-stderr-in-c
+                                fprintf(stderr, "%s with pid [%d] and jid [%d] exited normally\n", jobs_ptr[i]->cmd_name, jobs_ptr[i]->pid, jobs_ptr[i]->jid);
+                            }
+                            else
+                            {
+                                fprintf(stderr, "%s with pid [%d] and jid [%d] exited ABNORMALLY with status as %d\n", jobs_ptr[i]->cmd_name, jobs_ptr[i]->pid, jobs_ptr[i]->pid, WEXITSTATUS(stat_loc));
+                            }
                         }
                         else
                         {
-                            fprintf(stderr, "%s with pid [%d] and jid [%d] exited ABNORMALLY with status as %d\n", jobs_ptr[i]->cmd_name, jobs_ptr[i]->pid, jobs_ptr[i]->pid, WEXITSTATUS(*stat_loc));
+                            //some other change in state of concerned process
+                            /*WIFSTOPPED(wstatus)
+                          returns true if the child process was stopped by delivery of a
+                          signal; this is possible only if the call was done using WUN‐
+                              TRACED or when the child is being traced (see ptrace(2)).
+
+                     WSTOPSIG(wstatus)
+                         returns the number of the signal which caused the child to
+                      stop.  This macro should be employed only if WIFSTOPPED
+                          returned true.*/
+
+                            if (WIFSTOPPED(stat_loc))
+                            {
+                                int stopping_signal_number= WSTOPSIG(stat_loc);
+                                fprintf(stderr, "%s with pid [%d] and jid [%d] -> STOPPED with signal number %d\n", jobs_ptr[i]->cmd_name, jobs_ptr[i]->pid, jobs_ptr[i]->jid,stopping_signal_number);
+                            }
                         }
 
                         break;
